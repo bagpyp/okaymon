3a7d3edbc4f81678a357da6c8ab6f4c19b37fb23
Robert Cunningham <rcunni64@ford.com>
2022-01-16 09:08:28 -0800

    game

--- data.py
+++ data.py
@@ -4,19 +4,23 @@ from secrets import token_hex
 
 from settings import TOKEN_COMPLEXITY
 
+
 def path(type):
     return f"data/{type}.pkl"
+
 
 class Asset:
     id: str
     created: dt.datetime
     modified: dt.datetime
     type: str
+
     def __init__(self, type):
         self.type = type
         self.id = token_hex(TOKEN_COMPLEXITY)
         self.created = dt.datetime.now()
         self.modified = dt.datetime.now()
+
     def update(self):
         # read pickle
         tmp = pd.read_pickle(
@@ -24,19 +28,48 @@ class Asset:
         )
         # update state
         self.modified = dt.datetime.now()
-        for k,v in self.__dict__.items():
+        for k, v in self.__dict__.items():
             if k != 'id':
-                tmp.loc[self.id, k] = v 
+                tmp.loc[self.id, k] = v
         # persist
         tmp.to_pickle(
             path(self.type)
         )
 
+
 def pickle(assets: list[Asset]):
     pd.DataFrame(
-        [a.__dict__ for a in assets]
+        [
+            a.__dict__
+            for a in assets
+        ]
     ).set_index(
         'id'
     ).to_pickle(
-        path(assets[0].type)
+        path(
+            assets[0].type
+        )
+    )
+
+
+def batch_update(assets: list[Asset]):
+    df = pd.read_pickle(
+        path(
+            assets[0].type
+        )
+    )
+    df.update(
+        pd.DataFrame(
+            [
+                a.__dict__
+                for a in assets
+            ]
+        ).set_index(
+            'id'
+        )
+    )
+    df.to_pickle(
+        path(
+            assets[0].type
+        )
     )
--- game.py
+++ game.py
@@ -1,20 +1,34 @@
 #%% 
+import data
 import generator
 from okayball import Okayball
 from okaymon import Okaymon
 from player import Player
 
 class Game:
+    gen: int
     okayballs: list[Okayball]
     okaymon: list[Okaymon]
     players: list[Player]
+
+    def open_balls(self, gen):
+        for ball in filter(lambda b: b.gen == gen, self.okayballs):
+            ball.is_available = True
+        self.gen = gen
+        data.batch_update(self.okayballs)
+    def open_okaymon(self, gen):
+        for okaymon in filter(lambda b: b.gen == gen, self.okaymon):
+            okaymon.is_available = True
+        data.batch_update(self.okaymon)
+
     def __init__(self):
         self.okayballs = generator.generate_okayballs()
         self.okaymon = generator.generate_okaymon()
-        
+        self.players = generator.generate_players()
 
 if __name__ == '__main__':
     print('let the games begin!')
     game = Game()
     print(game.okayballs[:5])
     print(game.okaymon[:5])
+    game.open_balls(0)
